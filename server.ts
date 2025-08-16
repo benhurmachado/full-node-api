978abf846abe22ef5d8557efdfc7b3f1db6c35e8
Benhur <benhurteixeiramachado@gmail.com>
2025-08-16 16:59:11 -0300

    add: routes setup and validations

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import fastify from "fastify";
-import crypto from 'node:crypto';
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod";
+import { fastifySwagger } from '@fastify/swagger'
+import { createCourse } from "./src/routes/create-course.ts";
+import { getCourses } from "./src/routes/get-courses.ts";
+import { getCourseById } from "./src/routes/get-course-by-id.ts";
+import scalarApiReference from '@scalar/fastify-api-reference'
 
 const server = fastify({
     logger: {
@@ -11,7 +16,32 @@ const server = fastify({
             },
         },
     }
-});
+}).withTypeProvider<ZodTypeProvider>();
+
+if(process.env.NODE_ENV === 'development'){
+    server.register(fastifySwagger, {
+        openapi: {
+            info: {
+                title: 'Api Node',
+                version: '1.0.0'
+            }
+        },
+        transform: jsonSchemaTransform,
+    })
+
+    server.register(scalarApiReference, {
+        routePrefix: '/docs'
+    })
+}
+
+server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler);
+
+server.register(getCourses)
+server.register(getCourseById)
+server.register(createCourse)
+
+
 
 server.listen({ port: 3333 }).then(() => { 
     console.log("Server Port: 3333");
---
+++ src/routes/create-course.ts
@@ -0,0 +1,33 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { db } from '../database/client.ts'
+import { courses } from '../database/schema.ts'
+import z from 'zod'
+import { title } from 'process'
+
+export const createCourse: FastifyPluginAsyncZod = async (server) => {
+    server.post('/courses', {
+        schema: {
+            //swagger documentation
+            tags: ['courses'],
+            summary: 'Create a course',
+            description: 'This route receive a title and create a course in the db',
+            body: z.object({
+                title: z.string().min(5, "Title needs 5 chars at least")
+            }),
+            response: {
+                201: z.object({ courseId: z.uuid() }).describe('Success') 
+            }
+        }
+        }, 
+        async (request, reply) => {
+
+        const courseTitle = request.body.title
+
+        const result = await db
+            .insert(courses)
+            .values({ title: courseTitle })
+            .returning()
+
+        return reply.status(201).send({ courseId: result[0].id })
+    })
+}
